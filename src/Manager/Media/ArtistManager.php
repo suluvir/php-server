e5186330422357d821e44a44f85a71fd8f18d9d2
Jannis Fink <fink@uni-bremen.de>
2016-12-02 01:22:17 +0100

    fix update-db command

---
+++ src/Manager/Media/ArtistManager.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jannis
+ * Date: 01.12.16
+ * Time: 09:55
+ */
+
+namespace Suluvir\Manager\Media;
+
+
+class ArtistManager {
+
+}
---
+++ src/Schema/Media/Artist.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jannis
+ * Date: 01.12.16
+ * Time: 09:52
+ */
+
+namespace Suluvir\Schema\Media;
+
+
+class Artist {
+
+}
